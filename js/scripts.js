873e67685038ee0185e5059d4abe30e730db11e4
Vera weikel <vera.weikel@gmail.com>
2023-01-11 16:34:52 -0800

    It will add js file.
    Kai Clausen <kaiclausen123@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,16 @@ function hideCelebrityResultsAndError() {
 window.onload = function(){
   // fxn
   hideCelebrityResultsAndError();
-  document.querySelector("form").onubmit = function(event){
-    event.preventDefault();
-    
+  
+  document.querySelector("form").onsubmit = function(event){
+    hideCelebrityResultsAndError();
+
     // gather inputs
     const age = parseInt(document.getElementById("age-id").value);
     const height = parseInt(document.getElementById("height-id").value);
 
     if(!age && !height){
+      console.log("in the branch 1");
       document.getElementById("error-message").removeAttribute("class");
     } else {
       if(age > 60 && age>18){
@@ -24,10 +26,12 @@ window.onload = function(){
       } else if (age>18 && age<41 || height>72){
           document.getElementById("velma-dinkley").removeAttribute("class");
       } else if ((age>18 && age<41) && (height<=72 && !height===0)) {
-         document.getElementById("tarzan").removeAttribute("class");
+        document.getElementById("tarzan").removeAttribute("class");
       } else {
         document.getElementById("error-message").removeAttribute("class");
       }
     }
+
+    event.preventDefault();
   }
 }
